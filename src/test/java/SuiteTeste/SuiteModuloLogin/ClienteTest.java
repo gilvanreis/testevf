bb3992257439e6c9572f29141f8aed9338b71cec
gilvanreis <gilvanr7.0@gmail.com>
2019-11-27 22:29:19 -0300

    inserindo valor em campo com mascara de cpf

--- src/test/java/SuiteTeste/SuiteModuloLogin/ClienteTest.java
+++ src/test/java/SuiteTeste/SuiteModuloLogin/ClienteTest.java
@@ -4,11 +4,47 @@ import Core.BaseLoginTest;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  * Created by Gilvan Reis on 26/11/2019.
  */
 public class ClienteTest extends BaseLoginTest {
+    public void selectItemFromList(String fieldId, String value){
+        //Seleciona o Elemento pelo id
+        WebElement campo = driver.findElement(By.id(fieldId));
+        //Executa javascript para abrir Dropdown do select2 jquery
+        //Os oo só são carregados após abrir uma vez
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String OpenDropdownSript = "$('#"+fieldId+"').select2('open')";
+        js.executeScript(OpenDropdownSript, campo);
+        //Executa javascript para remover a classe que deixa o componente padrão de select hidden
+        String removeHiddenClassScript = "$('#"+fieldId+"').removeClass('select2-hidden-accessible')";
+        js.executeScript(removeHiddenClassScript, campo);
+        //Seleciona a opção a partir do componente padrão de select
+        Select combo = new Select(campo);
+        combo.selectByVisibleText(value);
+        //Executa javascript para fechar o Componente de Dropdown do select2
+        String closeDropdownScript = "$('#"+fieldId+"').select2('close')";
+        js.executeScript(closeDropdownScript, campo);
+    }
+    @Test
+    public void IncluirCliente_ComDocumentoCpfValido_DeveriaIncluirSucesso() {
+        driver.get("https://vfcli.varejofacil.com/cliente/cadastro?clearCache=true");
+        driver.findElement(By.linkText("Incluir")).click();
+        selectItemFromList("tipoPessoa","Física");
+        driver.findElement(By.id("cpfCnpjPassaporte")).click();
+        driver.findElement(By.id("cpfCnpjPassaporte")).sendKeys(Keys.HOME + "00666811385");
+
+
+
+        Assert.assertTrue(true);
+
+    }
+
     @Test
     public void PesquisaCliente_DeveriaClicarEmPesquisarSucesso() {
         driver.get("https://vfcli.varejofacil.com/cliente/cadastro?clearCache=true");
@@ -16,6 +52,7 @@ public class ClienteTest extends BaseLoginTest {
         Assert.assertTrue(true);
 
     }
+
     @Test
     public void PesquisaCliente_ComDocumentoCpfValido_DeveriaPesquisarSucesso() {
         driver.get("https://vfcli.varejofacil.com/cliente/cadastro?clearCache=true");
@@ -24,5 +61,7 @@ public class ClienteTest extends BaseLoginTest {
         Assert.assertTrue(true);
 
     }
+
+
 
 }
